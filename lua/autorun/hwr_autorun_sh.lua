92247264a96c370cae6993563909cc5b6a05d05f
ivan04 <ivanvladimir0435@gmail.com>
2020-06-12 22:17:23 -0600

    Added VJ section
    Yeah

--- lua/autorun/hwr_autorun_sh.lua
+++ lua/autorun/hwr_autorun_sh.lua
@@ -2,3 +2,22 @@ game.AddParticles( "particles/iv04_halowars_flamers.pcf" )
 
 PrecacheParticleSystem( "flame_halowars_flame" )
 PrecacheParticleSystem( "flame_halowars_napalm" )
+
+-- Yes yes there's a literal section of copyright of vrej's here but I'll skip it for now
+
+--------------------------------------------------*/
+------------------ Addon Information ------------------
+local PublicAddonName = "The public addon name goes here, Example: Dummy SNPCs"
+local AddonName = "The addon name goes here, Example: Dummy"
+local AddonType = "The addon type(s) Example: SNPC"
+local AutorunFile = "autorun/vj_as_autorun.lua"
+-------------------------------------------------------
+local VJExists = file.Exists("lua/autorun/vj_base_autorun.lua","GAME")
+if VJExists == true then
+	include('autorun/vj_controls.lua')
+
+	local vCat = "Halo Wars Resurgence"
+	
+	VJ.AddNPC("Flood Root","npc_vj_hw_flood_root",vCat )
+	
+end
