4cef3d9529b2ecee4436515940a1a9c3aa944fe3
ivan04 <ivanvladimir0435@gmail.com>
2020-07-02 16:06:41 -0600

    Visual fixes

--- lua/entities/npc_iv04_hw_flood_grunt/shared.lua
+++ lua/entities/npc_iv04_hw_flood_grunt/shared.lua
@@ -43,6 +43,25 @@ function ENT:OnInitialize()
 	table.insert(self.StuffToRunInCoroutine,func)
 end
 
+function ENT:Wander()
+	if self.IsControlled then return end
+	if math.random(1,2) == 1 then
+		self:WanderToPosition( (self:GetPos() + Vector( math.Rand( -1, 1 ), math.Rand( -1, 1 ), 0 ) * self.WanderDistance), self.WanderAnim[math.random(1,#self.WanderAnim)], self.MoveSpeed )
+	else
+		if self:GetActivity() != ACT_IDLE then
+			self:StartActivity(ACT_IDLE)
+		end
+		for i = 1, 5 do
+			self:SearchEnemy()
+			if !IsValid(self.Enemy) then
+				coroutine.wait(0.3)
+			else
+				return
+			end
+		end
+	end
+end
+
 function ENT:Shoot(ent)
 	ent = ent or self.Enemy
 	if !IsValid(ent) then return end
